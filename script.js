28c7b25e5176f6962fa55ef49d13712ec8efe35d
Bouncybull <vinceschoutrop@gmail.com>
2025-01-07 14:51:29 +0100

    bestellen en menu laat nu zien

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+let menuState = false;
+
 window.onload=function(){
 const myOverlay = document.querySelector ("#menuButton");
 
 myOverlay.addEventListener("click", openMenu);
 }
 function openMenu() {
-    console.log("its working");
+    if (menuState) {
+        document.getElementById('Menu').style.zIndex=-5;
+        menuState = false;
+    }
+    else
+    {
+        document.getElementById('Menu').style.zIndex=5;
+        menuState = true;
+    }
 }
