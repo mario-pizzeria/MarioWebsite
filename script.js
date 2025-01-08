584f29f9e6ace8ddabb089d9ebc50e13a1ee2ce8
Bouncybull <vinceschoutrop@gmail.com>
2025-01-08 14:59:16 +0100

    bestelling pagina en achtergrond gemaakt

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let menuState = false;
 
 window.onload=function(){
 const myOverlay = document.querySelector ("#menuButton");
+const backButt = document.querySelector("backarrow")
 
 myOverlay.addEventListener("click", openMenu);
+backButt.addEventListener("hover", buttHover);
 }
+
 function openMenu() {
     if (menuState) {
         document.getElementById('menu').style.zIndex=-5;
@@ -17,4 +20,8 @@ function openMenu() {
         document.getElementById('menuBackground').style.zIndex=4;
         menuState = true;
     }
+}
+
+function buttHover(){
+    document.getElementsByClassName("backarrow").src = "backarrow_hvr.png";
 }
