6a714badb8604707723f670cb99508dc8c8acb31
Bouncybull <vinceschoutrop@gmail.com>
2025-01-16 20:43:19 +0100

    communicatie met database en bestellen mogelijk gemaakt

--- bestelling.php
+++ bestelling.php
@@ -2,8 +2,8 @@
 
 $host = '127.0.0.1';
 $db = 'mariopizzeria';
-$user = 'extra';
-$pass = 'Frikandel@1';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $stmt = $pdo->prepare("INSERT INTO bestel_regel (product_id, bestel_id, aantal) VALUES (?, ?, ?)");
                     $stmt->execute([$product_id, $bestel_id, $aantal]);
 
-                    // Koppel pizzastatus automatisch
+                    //Koppel pizzastatus automatisch
                     $bestelRegel_id = $pdo->lastInsertId();
                     $status_id = 1; // Veronderstelde status_id (bijvoorbeeld: 'In voorbereiding')
                     $stmt = $pdo->prepare("INSERT INTO pizza_status (bestelRegel_id, status_id, changed_at) VALUES (?, ?, ?)");
@@ -67,70 +67,267 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // TRANSACTIE VOLTOOIEN
             $pdo->commit();
 
-            echo "Bestelling succesvol aangemaakt!";
+            echo "<p class='echo'> Bestelling succesvol aangemaakt! </p>";
         } catch (Exception $e) {
             // ROLLBACK bij een fout
             $pdo->rollBack();
-            echo "Fout bij het aanmaken van de bestelling: " . $e->getMessage();
+            echo "<p class='echo'> Fout bij het aanmaken van de bestelling: </p>" . $e->getMessage();
         }
     } else {
         echo "Vul alle velden correct in.";
     }
 } else {
-    echo "Formulier is niet correct ingediend.";
+    echo "<p class='echo'> Formulier is niet correct ingediend. </p>";
 }
 ?>
 
-<!DOCTYPE html>
+<html><!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzeria Bestelling</title>
+    <title>Reserveer - Mario & Luigi's Pizza's</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Pizzeria Bestelling</h2>
-    <form action="bestelling.php" method="POST">
-        <fieldset>
-            <legend>Klantgegevens</legend>
-            <label for="naam">Naam:</label><br>
-            <input type="text" id="naam" name="naam" required><br><br>
-            <label for="telefoonnummer">Telefoonnummer:</label><br>
-            <input type="text" id="telefoonnummer" name="telefoonnummer" required><br><br>
-        </fieldset>
-        <fieldset>
-            <legend>Bestelgegevens</legend>
-            <label for="product_1">Aantal Margherita:</label><br>
-            <input type="number" id="product_1" name="product_1" value="0" min="0"><br><br>
+    <section class="orderscreen">
+        <video src="backgroundvid.mp4" muted loop autoplay></video>
+        <img class= "chefbackground" src="chef_background.png" alt="chef in the background">
+        <img class="standardBackground" src="bestellen_background.png" alt="Menu background">
+        <div class="backgroundhue"></div>
+        <div class="line-1"></div>
+        <div class="line-2"></div>
+        <div>
+            <p class="numberbackground">02</p>
+            <p class="textbackground">Bestellen</p>
+        </div>
+        <div class="foodtype"> 
+            <ul>
+                <li id="type1">Pizza's</li>
+                <li id="type2">Vegetarisch</li>
+                <li id="type3">Drank</li>
+                <li id="type4">Desert</li>
+            </ul>
+        </div>
+        
+        <div class="message"></div>
 
-            <label for="product_2">Aantal Pepperoni:</label><br>
-            <input type="number" id="product_2" name="product_2" value="0" min="0"><br><br>
+        <form action="bestelling.php" method="POST">
 
-            <label for="product_3">Aantal Quattro Formaggi:</label><br>
-            <input type="number" id="product_3" name="product_3" value="0" min="0"><br><br>
+            <div class ="foodmenu">
+                <fieldset>
+                    <legend>Pizza's</legend>
+                    <label for="product_1">Aantal Margherita:</label><br>
+                    <input type="number" id="product_1" name="product_1" value="0" min="0"><br><br>
 
-            <label for="product_3">Aantal Hawaï:</label><br>
-            <input type="number" id="product_4" name="product_4" value="0" min="0"><br><br>
+                    <label for="product_2">Aantal Pepperoni:</label><br>
+                    <input type="number" id="product_2" name="product_2" value="0" min="0"><br><br>
 
-            <label for="product_3">Aantal Vegan Special:</label><br>
-            <input type="number" id="product_5" name="product_5" value="0" min="0"><br><br>
+                    <label for="product_3">Aantal Quattro Formaggi:</label><br>
+                    <input type="number" id="product_3" name="product_3" value="0" min="0"><br><br>
 
-            <label for="product_3">Aantal Coca-Cola:</label><br>
-            <input type="number" id="product_6" name="product_6" value="0" min="0"><br><br>
+                    <label for="product_3">Aantal Hawaï:</label><br>
+                    <input type="number" id="product_4" name="product_4" value="0" min="0"><br><br>
+            </div>
 
-            <label for="product_3">Aantal Fanta:</label><br>
-            <input type="number" id="product_7" name="product_7" value="0" min="0"><br><br>
+            <div class ="foodmenu">
+                <fieldset>
+                <legend>Vegetarisch</legend>
 
-            <label for="product_3">Aantal Sprite:</label><br>
-            <input type="number" id="product_8" name="product_8" value="0" min="0"><br><br>
+                <label for="product_3">Aantal Vegan Special:</label><br>
+                <input type="number" id="product_5" name="product_5" value="0" min="0"><br><br>
+                </fieldset>
+            </div>
 
-            <label for="product_3">Aantal Tiramisu:</label><br>
-            <input type="number" id="product_9" name="product_9" value="0" min="0"><br><br>
+            <div class ="foodmenu">
+                <fieldset>
+                    <legend>Drank</legend>
 
-            <label for="product_3">Aantal Panna Cotta:</label><br>
-            <input type="number" id="product_10" name="product_10" value="0" min="0"><br><br>
-        </fieldset>
-        <input type="submit" value="Bestellen">
+                    <label for="product_3">Aantal Coca-Cola:</label><br>
+                    <input type="number" id="product_6" name="product_6" value="0" min="0"><br><br>
+
+                    <label for="product_3">Aantal Fanta:</label><br>
+                    <input type="number" id="product_7" name="product_7" value="0" min="0"><br><br>
+
+                    <label for="product_3">Aantal Sprite:</label><br>
+                    <input type="number" id="product_8" name="product_8" value="0" min="0"><br><br>
+                </fieldset>
+            </div>
+            
+            <div class ="foodmenu" id="foodlist">
+                <fieldset>
+                <legend>Desert</legend>
+
+                <label for="product_3">Aantal Tiramisu:</label><br>
+                <input type="number" id="product_9" name="product_9" value="0" min="0"><br><br>
+
+                <label for="product_3">Aantal Panna Cotta:</label><br>
+                <input type="number" id="product_10" name="product_10" value="0" min="0"><br><br>
+                </fieldset>
+            </div>
+
+            <div id="overlay_cart">
+                <img src="overlaybackground.png" alt="">
+                <h1>Bestelling:</h1>
+                <legend>Klantgegevens</legend>
+                <label for="naam">Naam:</label><br>
+                <input type="text" id="naam" name="naam" required><br><br>
+                <label for="telefoonnummer">Telefoonnummer:</label><br>
+                <input type="text" id="telefoonnummer" name="telefoonnummer" required><br><br>
+                <h2>Producten:</h2>
+                <ul id="productenlijst">
+                </ul>
+                <p>Totaal Prijs:</p>
+                <p Id="totalPrice"></p>
+                <input type="submit" value="Bestellen">
+            </div> 
     </form>
+    <img id="cart" src="shoppingcart.png" alt="pictogram winkelwagen">
+    <a href="index.html"><img class="backarrow" src="backarrow_std.png" alt="Go back to main menu."></a>    
+    </section>
+
+<script>
+    const message = document.getElementsByClassName("message")[0];
+    const pizza = document.getElementsByClassName("foodmenu")[0];
+    const vega = document.getElementsByClassName("foodmenu")[1];
+    const drank = document.getElementsByClassName("foodmenu")[2];
+    const desert = document.getElementsByClassName("foodmenu")[3];
+    let typeID;
+    let cartOpen = false;
+    let x = 1;
+
+    window.onload=function(){
+
+        const typePizza = document.getElementById("type1");
+        const typeVega = document.getElementById("type2");
+        const typeDrank = document.getElementById("type3");
+        const typeDesert = document.getElementById("type4");
+        const cart = document.getElementById("cart");
+
+        let echo = document.getElementsByClassName("echo")[0];
+
+        typePizza.addEventListener("click", 
+            function(){
+                typeID = 1;
+                document.getElementById("type1").style.color="rgb(0, 0, 0)";
+                document.getElementById("type2").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type3").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type4").style.setProperty("color", "rgb(255, 255, 255)");
+                switchType()
+            });
+        typeVega.addEventListener("click",
+            function(){ 
+                typeID = 2;
+                document.getElementById("type2").style.setProperty("color", "rgb(0, 0, 0)");
+                document.getElementById("type1").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type3").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type4").style.setProperty("color", "rgb(255, 255, 255)");
+                switchType()
+            });
+        typeDrank.addEventListener("click", 
+            function(){ 
+                typeID = 3;
+                document.getElementById("type3").style.setProperty("color", "rgb(0, 0, 0)");
+                document.getElementById("type1").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type2").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type4").style.setProperty("color", "rgb(255, 255, 255)");
+                switchType()
+            });
+        typeDesert.addEventListener("click", 
+            function(){
+                typeID = 4;
+                document.getElementById("type4").style.setProperty("color", "rgb(0, 0, 0)");
+                document.getElementById("type1").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type2").style.setProperty("color", "rgb(255, 255, 255)");
+                document.getElementById("type3").style.setProperty("color", "rgb(255, 255, 255)");
+                switchType()
+            });
+        cart.addEventListener("click", openCart);
+        message.appendChild(echo);
+        }
+
+    function switchType()
+    {   
+
+        switch(typeID) {
+        case 1:
+            pizza.style.display="inline";
+            vega.style.display="none";
+            drank.style.display="none";
+            desert.style.display="none";
+            message.style.display="none";
+            break;
+        case 2:
+            pizza.style.display="none";
+            vega.style.display="inline";
+            drank.style.display="none";
+            desert.style.display="none";
+            message.style.display="none";
+            break;
+        case 3:
+            pizza.style.display="none";
+            vega.style.display="none";
+            drank.style.display="inline";
+            desert.style.display="none";
+            message.style.display="none";
+            break;
+        case 4:
+            pizza.style.display="none";
+            vega.style.display="none";
+            drank.style.display="none";
+            desert.style.display="inline";
+            message.style.display="none";
+            break;
+        }   
+    }
+
+
+    function openCart()
+    {
+        if(openCart)
+        {
+            document.getElementById("overlay_cart").style.display='none';
+            openCart = false;
+        }
+        else
+        {
+            document.getElementById("overlay_cart").style.display='inline';
+            openCart = true;
+            showPrice();
+        }
+    }
+
+    function showPrice()
+    {
+        let list = document.getElementById("productenlijst");
+
+        let margherita = document.getElementById("product_1");
+        let pepperoni = document.getElementById("product_2");
+        let quattro = document.getElementById("product_3");
+        let hawai = document.getElementById("product_4");
+        let veganspecial = document.getElementById("product_5");
+        let cocacola = document.getElementById("product_6");
+        let fanta = document.getElementById("product_7");
+        let sprite = document.getElementById("product_8");
+        let tiramisu = document.getElementById("product_9");
+        let pannacotta = document.getElementById("product_10");
+
+        let totalPrice = document.getElementById("totalPrice");
+
+        if(margherita.value > 0)
+            {
+                let newProduct = document.createElement("li");
+                let newName = document.createElement("p");
+                let newPrice = document.createElement("p");
+                let newCount = document.createElement("p");
+                newName.innerHTML = "Margherita";
+                newPrice.innerHTML = "€8,50";
+                newCount.innerHTML = margherita.value;
+                list.appendChild(newProduct);
+                newProduct.appendChild(newName);
+            }
+    }
+
+    </script>
 </body>
 </html>
